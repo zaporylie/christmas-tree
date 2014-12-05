52212f25032f478767ea009b930a9b08489bc1a2
Jakub Piasecki <jakub@piaseccy.pl>
2014-12-05 19:26:04 +0000

    Fixed #1 by zaporylie

--- webserver.py
+++ webserver.py
@@ -2,17 +2,23 @@ from flask import Flask, render_template, request
 import datetime, json
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-   # now = datetime.datetime.now()
-   # timeString = now.strftime("%Y-%m-%d %H:%M")
-   response = request.get_json()
-   string = response['ref']
-   templateData = {
-      'title' : 'HELLO!',
-      'ref': string
-      }
-   return render_template('main.html', **templateData)
+@app.route("/", methods=['GET'])
+def index():
+  return render_template('index.html')
+
+@app.route("/endpoint", methods=['GET', 'POST'])
+def endpoint():
+  if request.headers.get('X-GitHub-Event') is None:
+	  return "X-Github-Event header required", 400;
+
+  event = request.headers.get('X-GitHub-Event')
+
+  # Get JSON from input.
+  response = request.get_json()
+
+  ref = response['ref']
+
+  return 'ok', 200
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
